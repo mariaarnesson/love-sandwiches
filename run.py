454c900e1987003d7729bf0acc1a1764306faa5b
mariaarnesson <maria_arnesson@outlook.com>
2022-11-29 13:08:22 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -22,13 +17,21 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+        return sales_data
+
+
 
 
 def validate_data(values):
@@ -37,7 +40,6 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
-   
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -46,6 +48,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
